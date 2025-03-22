7bb9ac900dad292595b689f6ae585ddb2b9c3e16
AndrewOchs <tun78376@temple.edu>
2025-03-22 17:43:00 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,8 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  server: {
-    port: 3000,
-    open: true,
-  },
   build: {
-    outDir: 'dist',
-    minify: 'terser',
+    minify: 'esbuild', // Use esbuild instead of terser
     sourcemap: false,
   }
 });
